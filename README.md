023e0dfcbff00623c7191873c408f9e19421d65b
Pedro <pedropaulo_nunes@outlook.com>
2021-05-28 16:51:04 -0300

    [UPDATE]: Added functionality to 'app.py', updated the journey, and added 'passwords.txt'.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # password-generator
 First project.. yay!
 
-#Day One 1
+#Day One 
 This project is a basic one to my personal development, so I can get better little by little. I hope I improve my skills.
 
 Just realized how hard git is, and I am really struggling with it lol.
 
+#Day Two
+Finally mastered github, now I know that in order to update the remote
+repository i have to do some pushes :)
--- app.py
+++ app.py
@@ -1,3 +1,42 @@
-if __name__ == '__main__':
-    if True:
-        pass
+import random
+import string
+import hashlib
+
+abc = [le for le in string.ascii_letters]
+num = [num for num in string.digits]
+sym = ['@', '*', '#', '!', '%', '.', ',']
+
+
+desired_len = input("What is the size of the password you want to generate? Enter a number (1-65):\n")
+
+try:
+    desired_len = int(desired_len)
+    success = 1
+except ValueError:
+    success = None
+
+while success is None:
+    print('Invalid Number!')
+    desired_len = input("What is the size of the password you want to generate? Enter a number (1-65):\n")
+    try:
+        desired_len = int(desired_len)
+        success = 1
+    except ValueError:
+        success = None
+
+as_set = set()
+
+while len(as_set) < desired_len:
+    _type = random.choice([abc, num, sym])
+    char = random.choice(_type)
+
+    as_set.add(char)
+
+
+password = ''.join(as_set)
+
+with open('passwords.txt', 'a') as file:
+    file.write(f"{as_set}\n")
+
+
+print(f"Your password: {password}\n")
